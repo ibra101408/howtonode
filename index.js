779606b58aaa4d57806fc0d7379bdd4137c34f6c
Daniil <daniil.vodenejev@khk.ee>
2022-02-03 10:16:18 +0200

    html files update

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ var url = require('url');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-    fs.readFile('demofile1.html', function (err, data){
-        fs.appendFile('mynewfile.txt', 'Hello content!', function (err){
+
+        fs.appendFile('mynewfile1.txt', 'This is my text!', function (err){
             if (err) throw err;
-            console.log("Saved!");
+            console.log("Updated!");
         })
 
-    });
+        }).listen(8050);
 
 
 
 
 
- /*
+/*
+
+
+
+
+  fs.readFile('demofile1.html', function (err, data){
  res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         return res.end();
@@ -27,4 +32,4 @@ http.createServer(function (req, res) {
     res.write("     The date and time are currently: " + dt.myDataTime());
     res.end();
     res.end(txt);*/
-}).listen(8050);
+
