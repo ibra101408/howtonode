a1b9a4808c8f2a31f29a6e195fc9e3d276bb9383
Daniil <daniil.vodenejev@khk.ee>
2022-02-03 10:37:57 +0200

    URL Module

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 var http = require('http');
-var dt = require('./node_modules/module.js');
+//var dt = require('./node_modules/module.js');
 var url = require('url');
 var fs = require('fs');
-const {compileETag} = require("express/lib/utils");
-var adr = 'http://localhost:8050/default.html?year=2017&month=february';
-var q = url.parse(adr, true);
+//const {compileETag} = require("express/lib/utils");
+//var adr = 'http://localhost:8050/default.html?year=2017&month=february';
+
+http.createServer(function (req, res) {
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function (err, data){
+        if (err){
+            res.writeHead(404,{'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(8050);
+
+/*
+
 
 console.log(q.host);
 console.log(q.pathname);
@@ -14,13 +30,6 @@ var qdata = q.query;
 console.log(qdata.month);
 
 
-
-/*
-http.createServer(function (req, res) {
-
-
-
-        }).listen(8050);
 
  fs.rename('mynewfile1.txt', 'myrenamedfile.txt',function (err){
             if (err) throw err;
