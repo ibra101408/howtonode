435c05a1a7a72e771deecfc11aa49b01efdcc9cd
Daniil <daniil.vodenejev@khk.ee>
2022-02-03 10:08:18 +0200

    html files read and create

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 var http = require('http');
 var dt = require('./node_modules/module.js');
 var url = require('url');
+var fs = require('fs');
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.readFile('demofile1.html', function (err, data){
+        fs.appendFile('mynewfile.txt', 'Hello content!', function (err){
+            if (err) throw err;
+            console.log("Saved!");
+        })
 
-    var q = url.parse(req.url, true).query;
+    });
+
+
+
+
+
+ /*
+ res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+
+ var q = url.parse(req.url, true).query;
     var txt = q.year + " " + q.month;
     res.write(req.url);
-    res.end(txt);
- /*
-
-
     res.write("     The date and time are currently: " + dt.myDataTime());
-    res.end();*/
+    res.end();
+    res.end(txt);*/
 }).listen(8050);
