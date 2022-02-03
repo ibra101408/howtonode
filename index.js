6761277e4d05b1efed09b45e9800b30868c4ffd6
Daniil <daniil.vodenejev@khk.ee>
2022-02-03 09:57:43 +0200

    HTTP moodul

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var http = require('http');
 var dt = require('./node_modules/module.js');
+var url = require('url');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write("The date and time are currently: " + dt.myDataTime());
-    res.end();
+
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.write(req.url);
+    res.end(txt);
+ /*
+
+
+    res.write("     The date and time are currently: " + dt.myDataTime());
+    res.end();*/
 }).listen(8050);
